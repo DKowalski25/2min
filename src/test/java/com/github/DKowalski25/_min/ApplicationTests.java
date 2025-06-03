fac66469e80a38b22d0e353b0f5f9f81efa282ee
DKowalski25 <dshinoda25@gmail.com>
2025-06-03 16:43:02 +0300

    Initial commit

---
+++ src/test/java/com/github/DKowalski25/_min/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.DKowalski25._min;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
