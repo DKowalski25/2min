697e83a8bff7483c691132b77fc8e500eb92e722
DKowalski25 <dshinoda25@gmail.com>
2025-06-06 22:19:47 +0300

    U5 - Created UserDTO

---
+++ src/main/java/com/github/DKowalski25/_min/dto/user/UserResponseDTO.java
@@ -0,0 +1,11 @@
+package com.github.DKowalski25._min.dto.user;
+
+import com.github.DKowalski25._min.models.UserRole;
+
+public record UserResponseDTO (
+    Integer id,
+    String username,
+    String email,
+    UserRole role,
+    boolean isBlocked
+) {}
