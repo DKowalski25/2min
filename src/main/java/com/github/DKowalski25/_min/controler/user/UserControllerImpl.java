a84c055c5f0627c3aef2426d0a26ee12f873a7d5
DKowalski25 <dshinoda25@gmail.com>
2025-06-07 14:54:10 +0300

    U12 - Created UserController

---
+++ src/main/java/com/github/DKowalski25/_min/controler/user/UserControllerImpl.java
@@ -0,0 +1,68 @@
+package com.github.DKowalski25._min.controler.user;
+
+import com.github.DKowalski25._min.dto.user.UserRequestDTO;
+import com.github.DKowalski25._min.dto.user.UserResponseDTO;
+import com.github.DKowalski25._min.dto.user.UserUpdateDTO;
+import com.github.DKowalski25._min.service.user.UserService;
+
+import jakarta.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserControllerImpl implements UserController {
+
+    private final UserService userService;
+
+    @Override
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody @Valid UserRequestDTO userDto) {
+        UserResponseDTO createdUser = userService.createUser(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @Override
+    public ResponseEntity<UserResponseDTO> getUserById(Integer id) {
+        UserResponseDTO user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @Override
+    public ResponseEntity<UserResponseDTO> getUserByUsername(String username) {
+        UserResponseDTO user = userService.getUserByUsername(username);
+        return ResponseEntity.ok(user);
+    }
+
+    @Override
+    public ResponseEntity<UserResponseDTO> getUserByEmail(String email) {
+        UserResponseDTO user = userService.getUserByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+
+    @Override
+    public ResponseEntity<List<UserResponseDTO>> getAll() {
+        List<UserResponseDTO> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+
+    }
+
+    @Override
+    public ResponseEntity<Void> updateUser(Integer id, UserUpdateDTO userDto) {
+        userService.updateUser(userDto);
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteUser(Integer id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
