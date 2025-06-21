f1b7f7098c4c276ea59041a17a389a7a4cbd508b
DKowalski25 <dshinoda25@gmail.com>
2025-06-21 15:37:16 +0300

    UT-19 -- Fix generateToken method: Include userId in JWT token and update generateToken to accept User

--- src/main/java/com/github/DKowalski25/_min/controller/auth/AuthControllerImpl.java
+++ src/main/java/com/github/DKowalski25/_min/controller/auth/AuthControllerImpl.java
@@ -2,8 +2,10 @@ package com.github.DKowalski25._min.controller.auth;
 
 import com.github.DKowalski25._min.config.security.jwt.JwtUtil;
 import com.github.DKowalski25._min.dto.auth.LoginRequest;
+import com.github.DKowalski25._min.dto.user.UserMapper;
 import com.github.DKowalski25._min.dto.user.UserRequestDTO;
 import com.github.DKowalski25._min.dto.user.UserResponseDTO;
+import com.github.DKowalski25._min.models.User;
 import com.github.DKowalski25._min.service.user.UserService;
 
 import jakarta.validation.Valid;
@@ -15,7 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,20 +29,22 @@ public class AuthControllerImpl implements AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
     private final UserService userService;
+    private final UserMapper userMapper;
 
     @Override
     public ResponseEntity<String> login(@RequestBody LoginRequest request) {
         try {
-            System.out.println("Attempting login for: " + request.username());
+            User user = userMapper.toEntity(userService.getUserByUsername(request.username()));
+
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(request.username(), request.password())
             );
-            System.out.println("Authentication success for: " + request.username());
-            String token = jwtUtil.generateToken(request.username());
-            System.out.println("Generated token for: " + request.username());
+
+            String token = jwtUtil.generateToken(user);
+
             return ResponseEntity.ok(token);
         } catch (Exception e) {
-            System.err.println("Login failed for: " + request.username());
+
             e.printStackTrace();
             return ResponseEntity.status(500).body("Authentication failed");
         }
