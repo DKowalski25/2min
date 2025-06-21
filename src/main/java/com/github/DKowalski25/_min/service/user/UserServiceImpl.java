7beddc4ae81879721c61fd1ca13cd1953150f2c8
DKowalski25 <dshinoda25@gmail.com>
2025-06-22 01:23:21 +0300

    UT-21 -- Update method createUser: Now after saving the user, the method is called createDefaultTimeBlocks

--- src/main/java/com/github/DKowalski25/_min/service/user/UserServiceImpl.java
+++ src/main/java/com/github/DKowalski25/_min/service/user/UserServiceImpl.java
@@ -8,11 +8,14 @@ import com.github.DKowalski25._min.exceptions.BusinessValidationException;
 import com.github.DKowalski25._min.exceptions.EntityNotFoundException;
 import com.github.DKowalski25._min.models.User;
 import com.github.DKowalski25._min.repository.user.UserRepository;
+import com.github.DKowalski25._min.service.timeblock.TimeBlockService;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Service;
+
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -24,6 +27,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final TimeBlockService timeBlockService;
 
     @Override
     @Transactional
@@ -33,6 +37,7 @@ public class UserServiceImpl implements UserService {
         User user = userMapper.toEntity(userDTO);
         user.setPassword(passwordEncoder.encode(userDTO.password()));
         User savedUser = userRepository.save(user);
+        timeBlockService.createDefaultTimeBlocks(savedUser);
         return userMapper.toResponse(savedUser);
     }
 
