b64d236bbe33c546038934c7ec6ad50864cc5552
DKowalski25 <dshinoda25@gmail.com>
2025-08-19 22:17:03 +0500

    CAL-14 -- Added annotation @Mapping for methods field statEvent and endEvent

--- src/main/java/com/github/DKowalski25/_min/dto/calendar/CalendarEventMapper.java
+++ src/main/java/com/github/DKowalski25/_min/dto/calendar/CalendarEventMapper.java
@@ -2,14 +2,16 @@ package com.github.DKowalski25._min.dto.calendar;
 
 import com.github.DKowalski25._min.models.CalendarEvent;
 
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
+import javax.swing.*;
+
+@Mapper(componentModel = "spring")
 public interface CalendarEventMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "user", ignore = true)
+    @Mapping(target = "startEvent", expression = "java(LocalDateTime.parse(dto.start_event()))")
+    @Mapping(target = "endEvent", expression = "java(LocalDateTime.parse(dto.end_event()))")
     CalendarEvent toEntity(CalendarEventRequestDTO dto);
 
     CalendarEventResponseDTO toResponse(CalendarEvent calendarEvent);
