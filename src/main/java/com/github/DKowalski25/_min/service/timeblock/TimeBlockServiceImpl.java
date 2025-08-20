4519dd7f45d1158a9c3b9da6705c07c78075322b
DKowalski25 <dshinoda25@gmail.com>
2025-08-20 19:24:30 +0500

    R2.9 -- Added annotation @Transactional

--- src/main/java/com/github/DKowalski25/_min/service/timeblock/TimeBlockServiceImpl.java
+++ src/main/java/com/github/DKowalski25/_min/service/timeblock/TimeBlockServiceImpl.java
@@ -4,8 +4,11 @@ import com.github.DKowalski25._min.models.TimeBlock;
 import com.github.DKowalski25._min.models.TimeType;
 import com.github.DKowalski25._min.models.User;
 import com.github.DKowalski25._min.repository.timeblock.TimeBlockRepository;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +17,7 @@ import java.util.List;
 public class TimeBlockServiceImpl implements TimeBlockService {
     private final TimeBlockRepository timeBlockRepository;
     @Override
+    @Transactional
     public void createDefaultTimeBlocks(User user) {
         List<TimeType> types = List.of(TimeType.values());
         for (TimeType type: types) {
