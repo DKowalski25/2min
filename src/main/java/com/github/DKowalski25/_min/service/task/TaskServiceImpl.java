052c8ef10e915442ce108261e115ad27c9e934a5
DKowalski25 <dshinoda25@gmail.com>
2025-08-21 01:15:06 +0500

    R2.18-- Method updateTask now it compares not by sending, but by value.

--- src/main/java/com/github/DKowalski25/_min/service/task/TaskServiceImpl.java
+++ src/main/java/com/github/DKowalski25/_min/service/task/TaskServiceImpl.java
@@ -74,7 +74,7 @@ public class TaskServiceImpl implements TaskService {
         Task task = taskRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Task not found", id));
 
-        if (task.getUser().getId() != userId) {
+        if (!task.getUser().getId().equals(userId)) {
             throw new AccessDeniedException("You don't have permission to update this task");
         }
 
