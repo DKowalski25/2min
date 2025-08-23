47246c71efab215e9c5e93d674c2d921825436e0
DKowalski25 <dshinoda25@gmail.com>
2025-08-23 21:40:36 +0500

    ET16 -- Added new fields plannedForNext, archivedAt, createdAt, periodMarker

--- src/main/java/com/github/DKowalski25/_min/dto/task/TaskResponseDTO.java
+++ src/main/java/com/github/DKowalski25/_min/dto/task/TaskResponseDTO.java
@@ -3,6 +3,7 @@ package com.github.DKowalski25._min.dto.task;
 import com.github.DKowalski25._min.models.Tag;
 import com.github.DKowalski25._min.models.TimeType;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record TaskResponseDTO(
@@ -11,6 +12,10 @@ public record TaskResponseDTO(
         String description,
         Tag tag,
         boolean done,
+        boolean plannedForNext,
+        LocalDateTime archivedAt,
+        LocalDateTime createdAt,
+        String periodMarker,
         TimeType timeBlockType,
         UUID userId
 ) {}
